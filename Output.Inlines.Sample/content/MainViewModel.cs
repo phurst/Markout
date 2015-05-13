c3862f72fffed2783194ec1c2f9df488c77dbdf7
Nigel Taylor <nigel_taylor@phurst.com>
2015-05-13 15:54:59 -0400

    Added content directory and nuspec file

---
+++ Output.Inlines.Sample/content/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Documents;
+using Markout.Common.DataModel.Attribute;
+using Markout.Common.DataModel.Elements;
+using Markout.Input.Parser;
+
+namespace Markout.Output.Inlines.Sample {
+
+    public class MainViewModel : INotifyPropertyChanged {
+
+        private string _inputText;
+        private readonly MarkoutParser _markoutParser = new MarkoutParser();
+        private readonly MarkoutRenderer _renderer = new MarkoutRenderer();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel() {
+            MarkupInlines = new ObservableCollection<Inline>();
+        }
+
+        public string InputText {
+            get { return _inputText; }
+            set {
+                _inputText = value;
+                UpdateInlines();
+                NotifyPropertyChanged("InputText");
+            }
+        }
+
+        public ObservableCollection<Inline> MarkupInlines { get; set; }
+
+        public void Initialize() {
+            InputText = @"" +
+                        "plain {b}bold {u}bold underline {b}underline {i}underline italic {0}plain\n" +
+                        "{u}underline {u}{b}bold {0}plain\n" +
+                        "plain {a:http://google.com:LaunchUrl}Launch Google in Browser{a} plain\n" +
+                        "plain {a:SayHello:AnchorAction}Say \"Hello\" to me!{a} plain\n" +
+                        "plain {color:Red}Red {b}Red Bold {color}Bold {b}plain\n" +
+                        "plain {color:FF008000}Dark Green {b}Dark Green Bold {color}{b}plain\n" +
+                        "plain {font:Rockwell Extra Bold:24}This is Rockwell Extra Bold 24em in {color:red}Red{color} and {color:green}Green{color}\n" +
+                        "no color{font} plain.\n" +
+                        "plain {font:Rockwell Extra Bold:16:su}This is Rockwell Extra Bold 16em with strikeout and underline{font} plain.\n" +
+                        "";
+        }
+
+        private void UpdateInlines() {
+            IEnumerable<TextRun> textRuns = _markoutParser.Parse(InputText);
+            IEnumerable<Inline> inlines = _renderer.Render(textRuns, new Dictionary<string, Action<TextAttributeAnchor, string>> {
+                {
+                    "LaunchUrl",
+                    (anchor, content) => Process.Start(anchor.Uri.ToString())
+                },
+                {
+                    "AnchorAction",
+                    (anchor, content) => MessageBox.Show("Hello there! Content=" + (content ?? "NULL"))
+                },
+            });
+            MarkupInlines.Clear();
+            inlines.ToList().ForEach(inline => MarkupInlines.Add(inline));
+        }
+
+        private void NotifyPropertyChanged(string propertyName) {
+            PropertyChangedEventHandler h = PropertyChanged;
+            if (h != null) {
+                h(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Output.Inlines.Sample/content/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Markout.Output.Inlines.Sample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            Loaded += (sender, args) => {
+                MainViewModel vm = new MainViewModel();
+                DataContext = vm;
+                (new DispatcherTimer(
+                    new TimeSpan(0,0,0,0,500), 
+                    DispatcherPriority.Normal, 
+                    (s, e) => {
+                        ((DispatcherTimer)s).Stop();
+                        vm.Initialize();
+                    }, 
+                    Dispatcher.CurrentDispatcher)
+                    ).Start();
+            };
+        }
+    }
+}
